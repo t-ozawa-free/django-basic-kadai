63e05373aece3e7c59cd71e67f4c7f26ea1ad913
t-ozawa-free <t.ozawa.free@gmail.com>
2026-06-22 11:31:03 +0900

    課題012の修正対応

--- kadai_012/crud/views.py
+++ kadai_012/crud/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Product
 from django.urls import reverse_lazy
@@ -23,3 +23,6 @@ class ProductUpdateView(UpdateView):
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy('list')
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'crud/product_detail.html'
